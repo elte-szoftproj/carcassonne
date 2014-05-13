f514da0c3771146a9e4d50688ba45da8df309489
Zsolt Parragi <zsolt.parragi@kotelezo.hu>
2014-05-13 18:15:07 +0200

    basic deck

--- core/src/main/java/hu/elte/szoftproj/carcassonne/model/impl/BasicDeck.java
+++ core/src/main/java/hu/elte/szoftproj/carcassonne/model/impl/BasicDeck.java
@@ -14,6 +14,8 @@ public class BasicDeck implements Deck {
 
 	int remainingPieces;
 	
+	BasicTile starterTile;
+	
 	Map<BasicTile, Integer> remainingTiles;
 	List<BasicTile> randomHelper;
 	
@@ -23,6 +25,42 @@ public class BasicDeck implements Deck {
 		
 		randomHelper = new ArrayList<>(remainingTiles.keySet());
 		Collections.shuffle(randomHelper);
+		
+		starterTile = createTile("v1", "FCCCF-RRR-FFFFF"); 
+		
+		addTile(createTile("1", "CCCCC-CCC-CCCCC"), 1);
+		addTile(createTile("1", "CCCCC-CCC-CFFFC"), 4);
+		addTile(createTile("1", "CFFFC-CCC-CFFFC"), 3);
+		addTile(createTile("1", "CCCCF-CFF-CFFFF"), 5);
+		addTile(createTile("1", "FCCCC-FFC-FFFFC"), 2); // BAD, REWRITE: this is two city!
+		addTile(createTile("1", "CFFFC-CFC-CFFFC"), 3);
+		addTile(createTile("1", "FCCCF-FFF-FFFFF"), 3);
+		
+		addTile(createTile("1", "CCCCC-CCC-CFRFC"), 3);
+		addTile(createTile("1", "CCCCF-CFR-CFRFF"), 5); // BAD, REWRITE: roads are connected!!!
+		addTile(createTile("1", "FCCCF-FRR-FFRFF"), 3);
+		addTile(createTile("1", "FCCCF-RRF-FFRFF"), 3);
+		addTile(createTile("1", "FCCCF-RFR-FFRFF"), 3);
+		addTile(starterTile, 3);
+		
+		addTile(createTile("1", "FFRFF-RXR-FFRFF"), 1);
+		addTile(createTile("1", "FFFFF-RXR-FFRFF"), 4);
+		addTile(createTile("1", "FFRFF-FRF-FFRFF"), 8);
+		addTile(createTile("1", "FFFFF-RRF-FFRFF"), 9);
+		
+		addTile(createTile("1", "FFFFF-FMF-FFFFF"), 4);
+		addTile(createTile("1", "FFFFF-FMF-FFRFF"), 2);
+		
+	}
+	
+	protected void addTile(BasicTile t, int num) {
+		remainingTiles.put(t, new Integer(num));
+		remainingPieces+=num;
+		randomHelper.add(t);
+	}
+	
+	protected BasicTile createTile(String fileName, String config) {
+		return null;
 	}
 	
 	@Override
