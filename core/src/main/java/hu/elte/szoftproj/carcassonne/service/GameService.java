e8dde6827ae8e9330a820ad002e3d2600ef0de30
Zsolt Parragi <zsolt.parragi@cancellar.hu>
2014-12-08 20:43:11 +0100

    gameservice interface

--- core/src/main/java/hu/elte/szoftproj/carcassonne/service/GameService.java
+++ core/src/main/java/hu/elte/szoftproj/carcassonne/service/GameService.java
@@ -1,5 +1,13 @@
 package hu.elte.szoftproj.carcassonne.service;
 
-public class GameService {
+import hu.elte.szoftproj.carcassonne.domain.*;
+
+public interface GameService {
+
+    public Game getGameById(String gameId);
+
+    public Game placeTile(Game g, Player owner, Tile t, Rotation r, int y, int x);
+
+    public Game placeFollower(Game g, Player owner, Follower f, int y, int x, int dx, int dy);
 
 }
