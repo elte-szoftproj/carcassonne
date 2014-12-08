cf2929186497fe4f5f25e172891ed749393d24ba
Zsolt Parragi <zsolt.parragi@cancellar.hu>
2014-12-09 00:53:44 +0100

    added lobbyWDao test

---
+++ core/src/test/java/hu/elte/szoftproj/carcassonne/service/impl/wdao/lobbyServiceImplWDaoTest.java
@@ -0,0 +1,29 @@
+package hu.elte.szoftproj.carcassonne.service.impl.wdao;
+
+import hu.elte.szoftproj.carcassonne.config.ApplicationConfig;
+import hu.elte.szoftproj.carcassonne.service.LobbyServiceTest;
+import hu.elte.szoftproj.carcassonne.service.impl.LobbyServiceImplWDao;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.AnnotationConfigWebContextLoader;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(loader=AnnotationConfigWebContextLoader.class, classes=ApplicationConfig.class)
+@WebAppConfiguration
+@DirtiesContext(classMode=DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+public class lobbyServiceImplWDaoTest extends LobbyServiceTest {
+
+    @Autowired
+    private LobbyServiceImplWDao service;
+
+    @Before
+    public void initLobby() throws Exception {
+        serviceToTest = service;
+    }
+
+}
